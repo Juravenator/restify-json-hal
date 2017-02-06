f60525543cec36d3a7b98f97e91e71f5e2bd20bb
Glenn Dirkx <glenn.dirkx@gmail.com>
2017-02-06 12:02:07 +0100

    🛠 cleanRoutePieces now works correctly when visiting a 'some/path/?query' path

--- cleanRoutePieces.js
+++ cleanRoutePieces.js
@@ -2,10 +2,11 @@
 // used to make /api/call/ and /api/call equivalent
 module.exports = array => {
   var lastItem = array[array.length-1];
+  if (lastItem.indexOf("?") > -1) {
+    array[array.length-1] = lastItem.substring(0, lastItem.indexOf("?"));
+  }
   if (!lastItem || lastItem == "") {
     array.pop();
-  } else if (lastItem.indexOf("?") > -1) {
-    array[array.length-1] = lastItem.substring(0, lastItem.indexOf("?"));
   }
   return array;
 }
